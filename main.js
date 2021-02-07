32199f3b3900e342462418781a8a6ab3545a6d57
VladimirSh6892 <vladimirsh6892@gmail.com>
2021-02-07 23:38:57 +0300

    add "let" for 21,27,34 line

--- main.js
+++ main.js
@@ -18,20 +18,20 @@ console.log(Boolean(test)); //  output true
 
 const arr1 = [2, 3, 5, 8];
 let result = 1;
-for (i = 0; i < arr1.length; i++) {
+for (let i = 0; i < arr1.length; i++) {
   result *= arr1[i];
 }
 console.log(result);
 
 const arr2 = [2, 5, 8, 15, 0, 6, 20, 3];
-for (i = 0; i < arr2.length; i++) {
+for (let i = 0; i < arr2.length; i++) {
   if (arr2[i] > 5 && arr2[i] < 10) {
     console.log(arr2[i]);
   }
 }
 
 const arr3 = [2, 5, 8, 15, 0, 6, 20, 3];
-for (i = 0; i < arr3.length; i++) {
+for (let i = 0; i < arr3.length; i++) {
   if (!(arr3[i] % 2)) {
     console.log(arr3[i]);
   }
