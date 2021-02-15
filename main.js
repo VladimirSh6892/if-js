afbfefc33cf3bbcca22fcc1fccfea5a5a8a74993
VladimirSh6892 <vladimirsh6892@gmail.com>
2021-02-15 18:45:44 +0300

    add find object

--- main.js
+++ main.js
@@ -88,3 +88,59 @@ function sum(a) {
   };
 }
 console.log(sum(5)(2));
+
+const data = [
+  {
+    country: 'Russia',
+    city: 'Saint Petersburg',
+    hotel: 'Hotel Leopold',
+  },
+  {
+    country: 'Spain',
+    city: 'Santa Cruz de Tenerife',
+    hotel: 'Apartment Sunshine',
+  },
+  {
+    country: 'Slowakia',
+    city: 'Vysokie Tatry',
+    hotel: 'Villa Kunerad',
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hostel Friendship',
+  },
+  {
+    country: 'Indonesia',
+    city: 'Bali',
+    hotel: 'Ubud Bali Resort&SPA',
+  },
+  {
+    country: 'Netherlands',
+    city: 'Rotterdam',
+    hotel: 'King Kong Hostel',
+  },
+  {
+    country: 'Marocco',
+    city: 'Ourika',
+    hotel: 'Rokoko Hotel',
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hotel Rehberge Berlin Mitte',
+  },
+];
+const replaceStrForSearch = (str) => str.toLowerCase().replaceAll(' ', '');
+
+const search = (str) => {
+  const searchStr = replaceStrForSearch(str);
+
+  return data
+    .filter((object) => {
+      const string = `${object.country}${object.city}${object.hotel}`;
+      return replaceStrForSearch(string).includes(searchStr);
+    }).map((object) => `Страна: ${object.country} Город: ${object.city} Отель: ${object.hotel}`);
+};
+
+console.log(search('net '));
