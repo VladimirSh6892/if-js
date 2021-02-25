05fd04ef4baa5bab95f6a5b50d264e5644a3ef79
Vladimir <74614891+VladimirSh6892@users.noreply.github.com>
2021-02-25 20:56:06 +0300

    Lessons6 (#6)
    * add object placement
    
    * add palindrome String
    
    * func city of country

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 //  lessons2
-
 let user = 'John Doe';
 console.log(user); //   output John Doe
 const student = 'Vladimir';
@@ -39,10 +38,14 @@ for (let i = 0; i < arr3.length; i++) {
 
 //  lessons3
 
-const palindrome = (str) => {
-  str.toLowerCase();
-  return str === str.split('').reverse().join('') ? 'True' : 'False';
-};
+function palindrome(text) {
+  for (let i = 0; i < text.length / 2; i += 1) {
+    if (text[i] !== text[text.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
 console.log(palindrome('noon'));
 
 function min(a, b) {
@@ -179,3 +182,196 @@ const date = '2020-11-26';
 const dateFormat = (date2) => date2.split('-').reverse().join('.');
 
 console.log(dateFormat(date));
+
+//  lessons6
+
+const hotels = [
+  {
+    name: 'Hotel Leopold',
+    city: 'Saint Petersburg',
+    country: 'Russia',
+  },
+  {
+    name: 'Apartment Sunshine',
+    city: 'Santa Cruz de Tenerife',
+    country: 'Spain',
+  },
+  {
+    name: 'Villa Kunerad',
+    city: 'Vysokie Tatry',
+    country: 'Slowakia',
+  },
+  {
+    name: 'Hostel Friendship',
+    city: 'Berlin',
+    country: 'Germany',
+  },
+  {
+    name: 'Radisson Blu Hotel',
+    city: 'Kyiv',
+    country: 'Ukraine',
+  },
+  {
+    name: 'Paradise Hotel',
+    city: 'Guadalupe',
+    country: 'Mexico',
+  },
+  {
+    name: 'Hotel Grindewald',
+    city: 'Interlaken',
+    country: 'Switzerland',
+  },
+  {
+    name: 'The Andaman Resort',
+    city: 'Port Dickson',
+    country: 'Malaysia',
+  },
+  {
+    name: 'Virgin Hotel',
+    city: 'Chicago',
+    country: 'USA',
+  },
+  {
+    name: 'Grand Beach Resort',
+    city: 'Dubai',
+    country: 'United Arab Emirates',
+  },
+  {
+    name: 'Shilla Stay',
+    city: 'Seoul',
+    country: 'South Korea',
+  },
+  {
+    name: 'San Firenze Suites',
+    city: 'Florence',
+    country: 'Italy',
+  },
+  {
+    name: 'The Andaman Resort',
+    city: 'Port Dickson',
+    country: 'Malaysia',
+  },
+  {
+    name: 'Black Penny Villas',
+    city: 'BTDC, Nuca Dua',
+    country: 'Indonesia',
+  },
+  {
+    name: 'Koko Hotel',
+    city: 'Tokyo',
+    country: 'Japan',
+  },
+  {
+    name: 'Ramada Plaza',
+    city: 'Istanbul',
+    country: 'Turkey',
+  },
+  {
+    name: 'Waikiki Resort Hotel',
+    city: 'Hawaii',
+    country: 'USA',
+  },
+  {
+    name: 'Puro Hotel',
+    city: 'Krakow',
+    country: 'Poland',
+  },
+  {
+    name: 'Asma Suites',
+    city: 'Santorini',
+    country: 'Greece',
+  },
+  {
+    name: 'Cityden Apartments',
+    city: 'Amsterdam',
+    country: 'Netherlands',
+  },
+  {
+    name: 'Mandarin Oriental',
+    city: 'Miami',
+    country: 'USA',
+  },
+  {
+    name: 'Concept Terrace Hotel',
+    city: 'Rome',
+    country: 'Italy',
+  },
+  {
+    name: 'Ponta Mar Hotel',
+    city: 'Fortaleza',
+    country: 'Brazil',
+  },
+  {
+    name: 'Four Seasons Hotel',
+    city: 'Sydney',
+    country: 'Australia',
+  },
+  {
+    name: 'Le Meridien',
+    city: 'Nice',
+    country: 'France',
+  },
+  {
+    name: 'Apart Neptun',
+    city: 'Gdansk',
+    country: 'Poland',
+  },
+  {
+    name: 'Lux Isla',
+    city: 'Ibiza',
+    country: 'Spain',
+  },
+  {
+    name: 'Nox Hostel',
+    city: 'London',
+    country: 'UK',
+  },
+  {
+    name: 'Leonardo Vienna',
+    city: 'Vienna',
+    country: 'Austria',
+  },
+  {
+    name: 'Adagio Aparthotel',
+    city: 'Edinburgh',
+    country: 'UK',
+  },
+  {
+    name: 'Steigenberger Hotel',
+    city: 'Hamburg',
+    country: 'Germany',
+  },
+];
+function searchHotel(searchHotels) {
+  const res = [];
+  const serchStringLowerCase = searchHotels.replaceAll(' ', '').toLowerCase();
+  hotels.forEach((object) => {
+    const { country, city, name } = object;
+    const srcString = `${country}, ${city}, ${name}`;
+    if (checkStringTolowerCase(srcString, serchStringLowerCase)) {
+      res.push(`Страна: ${country}, Город: ${city}, Отель: ${name}`);
+    }
+  });
+  return res;
+}
+console.log(searchHotel('stei '));
+
+function searchCity() {
+  return hotels.reduce((accumulator, currentValue) => {
+    const resultSearch = [];
+    if (!accumulator[currentValue.country]) {
+      resultSearch.push(currentValue.city);
+      accumulator[currentValue.country] = resultSearch;
+    } else if (!accumulator[currentValue.country].includes(currentValue.city)) {
+      accumulator[currentValue.country].push(currentValue.city);
+    }
+    return accumulator;
+  });
+}
+console.log(searchCity());
+
+const palindromeString = (str) => {
+  str.toLowerCase();
+  return str === str.split('').reverse().join('') ? 'True' : 'False';
+};
+console.log(palindromeString('noon'));
