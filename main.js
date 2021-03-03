e6d9619d5f15662293eb7c8ebbd005d7cedc8f36
VladimirSh6892 <vladimirsh6892@gmail.com>
2021-03-03 20:07:17 +0300

    add const studentData

--- main.js
+++ main.js
@@ -447,3 +447,32 @@ const deepEqual = (object1, object2) => {
 
 console.log(deepEqual(obj1, obj2));
 console.log(deepEqual(obj1, obj3));
+
+//  lessons8
+
+const studentsData = [
+  {
+    firstName: 'Василий',
+    lastName: 'Петров',
+    admissionYear: 2019,
+    courseName: 'Java',
+  },
+  {
+    firstName: 'Иван',
+    lastName: 'Иванов',
+    admissionYear: 2018,
+    courseName: 'JavaScript',
+  },
+  {
+    firstName: 'Александр',
+    lastName: 'Федоров',
+    admissionYear: 2017,
+    courseName: 'Python',
+  },
+  {
+    firstName: 'Николай',
+    lastName: 'Петров',
+    admissionYear: 2019,
+    courseName: 'Android',
+  }
+];
