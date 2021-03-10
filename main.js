e55b9ab59f88d98c9f43add64c1fc7d726fd430e
VladimirSh6892 <vladimirsh6892@gmail.com>
2021-03-10 20:29:45 +0300

    completed working with classes

--- main.js
+++ main.js
@@ -474,5 +474,44 @@ const studentsData = [
     lastName: 'Петров',
     admissionYear: 2019,
     courseName: 'Android',
-  }
+  },
 ];
+class User {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+class Student extends User {
+  constructor(firstName, lastName, admissionYear, courseName) {
+    super(firstName, lastName);
+    this.admissionYear = admissionYear;
+    this.courseName = courseName;
+  }
+
+  get course() {
+    return new Date().getFullYear() - this.admissionYear;
+  }
+}
+class Students {
+  constructor(students) {
+    this.students = students.map(({
+      firstName, lastName, admissionYear, courseName,
+    }) => new Student(firstName, lastName, admissionYear, courseName));
+  }
+
+  getInfo() {
+    this.students.sort((prev, next) => prev.course - next.course);
+    const arrgument = [];
+    this.students.forEach((item) => {
+      arrgument.push(`${item.fullName} - ${item.courseName}, ${item.course} курс`);
+    });
+    return arrgument;
+  }
+}
+const students = new Students(studentsData);
+console.log(students.getInfo());
